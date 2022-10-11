9f029de3be98e2dae0f07fedfa3b1c71d3703451
wooodhead <jcwsw129@gmail.com>
2022-10-12 00:14:59 +0800

    feat: build ssr 成功

---
+++ packages/theme-doc/pages/_theme.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+// import { createTheme, defaultSideNavs } from 'vite-pages-theme-doc2'
+import { createTheme, defaultSideNavs } from '../src'
+import './index.css'
+import Component404 from './404'
+import { footerConfig } from './themConfig/footer'
+
+export default createTheme({
+  topNavs: [
+    {
+      label: 'Github',
+      href: 'https://github.com/vitejs/vite-plugin-react-pages',
+      icon: '⭐',
+    },
+  ],
+  logo: <div style={{ fontSize: '20px' }}>📘 Vite Pages</div>,
+  sideNavs(ctx) {
+    return defaultSideNavs(ctx, {
+      groupConfig: {
+        '/': {
+          advanced: {
+            label: 'Advanced',
+            order: 2,
+          },
+          'upgrade-guides': {
+            label: 'Upgrade Guides',
+            order: 3,
+          },
+        },
+      },
+    })
+  },
+  footer: ({ resolvedLocale: { localeKey } }) => {
+    return footerConfig.en
+  },
+  Component404,
+})
